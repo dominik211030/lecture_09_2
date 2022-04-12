f3c486a87f3ebaf875df09a1054a06aecbe434b6
dominik211030 <dompsik@gmail.com>
2022-04-12 09:15:46 +0200

    x

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,11 +12,23 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+
+
+
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path) as data_file:
+        data = json.load(data_file)
+
+    if field in data:
+        return data[field]
+    else:
+        return None
+
 
 
 def main():
-    pass
+    neserazena = read_data("sequential.json","unordered_numbers")
+    print(neserazena)
 
 
 if __name__ == '__main__':
